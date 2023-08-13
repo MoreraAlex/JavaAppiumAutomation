237a6caf46b5de5da0c24b1fd2dda81fc9a73dff
MoreraAlex <Moreraqa@gmail.com>
2023-08-13 15:12:47 +0300

    Test commit

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -1,4 +1,5 @@
 public class MainClass {
+    //Объявил static тк в данном случае создание объекта класса показалось излишним
     public static int getLocalNumber(){
         return 14;
     }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -6,6 +6,7 @@ public class MainClassTest extends MainClass
     @Test
     public void testGetLocalNumber()
     {
+        //Для большей читаемости кода вынес в отдельные переменные ОР
         int expected = 14;
         int actual = MainClass.getLocalNumber();
         Assert.assertTrue("Actual result is not equal to 14", actual == expected);
