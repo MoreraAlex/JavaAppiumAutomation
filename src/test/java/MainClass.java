093168908967f1ab42a0a9e190c152a943d53f0a
MoreraAlex <Moreraqa@gmail.com>
2023-08-13 15:25:56 +0300

    Test 2

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -1,6 +1,15 @@
 public class MainClass {
+    //Тест 2: Создал приватное поле класса и публичный метод возвращающий переменную
+    private int class_number = 20;
+    public int getClassNumber(){
+        return class_number;
+    }
+
+
     //Объявил static тк в данном случае создание объекта класса показалось излишним
     public static int getLocalNumber(){
         return 14;
     }
+
+
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -3,12 +3,21 @@ import org.junit.Assert;
 
 public class MainClassTest extends MainClass
 {
+    //Тест 2: проверка что метод getClassNumber возвращает > 45
+    @Test
+    public void testGetClassNumber()
+    {
+        MainClass classNum = new MainClass();
+        int actual = classNum.getClassNumber();
+        Assert.assertTrue("Actual number lower than 45", actual > 45);
+    }
+
     @Test
     public void testGetLocalNumber()
     {
         //Для большей читаемости кода вынес в отдельные переменные ОР
         int expected = 14;
         int actual = MainClass.getLocalNumber();
-        Assert.assertTrue("Actual result is not equal to 14", actual == expected);
+        Assert.assertEquals("Actual result is not equal to 14", actual, expected);
     }
 }
